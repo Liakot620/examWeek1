56b2ef67f5a201dd584992a77fb63416f5e0fd1a
liakot hossain dhali <dhaliliakot@gmail.com>
2024-10-16 23:11:12 +0600

    add update

--- student_result/result.php
+++ student_result/result.php
@@ -2,22 +2,23 @@
 // Initialize grades for 5 subjects
 $subject1 = 53;
 $subject2 = 63;
-$subject3 =34;
+$subject3 = 74;
 $subject4 = 34;  
-$subject5 = 62;
+$subject5 = 56;
 
 // Total and average variables
 $total_marks = 0;
 $average_marks = 0;
 
-//mark range
-if (($subject1 >=0 && $subject1<=100) &&  ($subject2 >=0 && $subject2<=100)&&  ($subject3 >=0 && $subject3<=100)&&  ($subject4 >=0 && $subject4<=100)&&  ($subject5 >=0 && $subject5<=100)){
+function myResult($subject1,$subject2,$subject3,$subject4,$subject5){
+
+    //mark range
+    if (($subject1 >=0 && $subject1<=100) &&  ($subject2 >=0 && $subject2<=100)&&  ($subject3 >=0 && $subject3<=100)&&  ($subject4 >=0 && $subject4<=100)&&  ($subject5 >=0 && $subject5<=100)){
 
     //fail condition
     if(($subject1>=33) && ($subject2>=33)&&  ($subject3>=33) && ($subject4>=33)&&($subject5>=33))
     
     {
-
             $total_marks=$subject1+$subject2+$subject3+$subject4+$subject5;
             $average_marks=$total_marks/5;
 
@@ -25,49 +26,39 @@ if (($subject1 >=0 && $subject1<=100) &&  ($subject2 >=0 && $subject2<=100)&&  (
 
             switch ($average_marks) {
                 case ($average_marks>=80 && $average_marks<=100):
-                    echo "Total Marks: {$total_marks}<br>";
-                    echo "Average Marks: {$average_marks}<br>";
-                    echo "Grade : A+";
+                    return "Total Marks: {$total_marks}<br>"."Average Marks: {$average_marks}<br>"."Grade: A+";
                     break;
                 case ($average_marks>=70 && $average_marks<80):
-                    echo "Total Marks: {$total_marks}<br>";
-                    echo "Average Marks: {$average_marks}<br>";
-                        echo "Grade : A";
+                    return "Total Marks: {$total_marks}<br>"."Average Marks: {$average_marks}<br>"."Grade: A";
                     break;
                 case ($average_marks>=60 && $average_marks<70):
-                    echo "Total Marks: {$total_marks}<br>";
-                    echo "Average Marks: {$average_marks}<br>";
-                        echo "Grade : A-";
+                    return "Total Marks: {$total_marks}<br>"."Average Marks: {$average_marks}<br>"."Grade: A-";
                     break;
                 case ($average_marks>=50 && $average_marks<60):
-                    echo "Total Marks: {$total_marks}<br>";
-                    echo "Average Marks: {$average_marks}<br>";
-                        echo "Grade : B";
+                    return "Total Marks: {$total_marks}<br>"."Average Marks: {$average_marks}<br>"."Grade: B";
                     break;
                 case ($average_marks>=40 && $average_marks<50):
-                    echo "Total Marks: {$total_marks}<br>";
-                    echo "Average Marks: {$average_marks}<br>";
-                        echo "Grade : C";
+                    return "Total Marks: {$total_marks}<br>"."Average Marks: {$average_marks}<br>"."Grade: C";
                     break;
                 case ($average_marks>=33 && $average_marks<40):
-                    echo "Total Marks: {$total_marks}<br>";
-                    echo "Average Marks: {$average_marks}<br>";
-                        echo "Grade : D";
+                    
+                  return "Total Marks: {$total_marks}<br>"."Average Marks: {$average_marks}<br>"."Grade: D";
                     break;
                 
                 default:
-                    echo "Grade : F";
+                    return "Grade : F";
         }
         
     }
     else{
-        echo "Grade : F";
+        return  "Grade : F";
     }
 }
 
 else{
-echo "Invalid marks range between 0 and 100.";
+   return "Invalid marks range between 0 and 100.";
+ }
 }
-
-
+$result=myResult($subject1,$subject2,$subject3,$subject4,$subject5);
+echo $result;
 ?>
